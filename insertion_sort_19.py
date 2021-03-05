d9e905934ffa56e6f942a9f26f442bda71da4aa6
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-03-05 13:11:47 -0600

    first commit

---
+++ insertion_sort_19.py
@@ -0,0 +1,16 @@
+arr = [31, 41, 59, 26, 41, 58]
+
+for i in range(1, len(arr)):
+ 
+        key = arr[i]
+ 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+
+print(arr)
