b7c52d4f26685326f2472bb04fd1e67db045680b
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-03-24 12:27:01 -0500

    added the anagram exalple

--- fancy_format.py
+++ fancy_format.py
@@ -3,7 +3,21 @@ import sys
 n = 10
 data = []
 for i in range(n):
+    
+    # seems to be starting at 0
     a = len(data)
     b = sys.getsizeof(data)
 
     print('Length: {0:3d}; Size in bytes: {1:4d}'. format(a,b))
+    data.append(n)
+    # this ran to slo wit was a O(n) running in linear time
+
+    # my anagramp exmaple 
+    def anagram(s1, s2):
+        # remove the spaces and lowcase letters
+        s1 = s1.replace(' ', '').lower()
+        s2 = s2.replace(' ', '').lower()
+
+        return sorted(s1) == sorted(s2)
+
+        
