9718f2ba7fae1ff98c1de8043e2e7c1228e8ebbd
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-03-25 13:14:06 -0500

    finished the inter anagram

--- fancy_format.py
+++ fancy_format.py
@@ -12,12 +12,58 @@ for i in range(n):
     data.append(n)
     # this ran to slo wit was a O(n) running in linear time
 
-    # my anagramp exmaple 
-    def anagram(s1, s2):
-        # remove the spaces and lowcase letters
-        s1 = s1.replace(' ', '').lower()
-        s2 = s2.replace(' ', '').lower()
 
-        return sorted(s1) == sorted(s2)
+
+    # my anagramp exmaple 
+def anagram(s1, s2):
+        # remove the spaces and lowcase letters
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+    print(sorted(s1), sorted(s2))
+    # the sorted function is what helped us find the answer!
+
+    return sorted(s1) == sorted(s2)
+    
+
+print(anagram('cast', 'saCt'))
+# not the best solution because it is using a python module .replcace
+
+
+def inter_anagram(s1, s2):
+    """will check to see if two strings are a secert message"""
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+    if len(s1) != len(s2):
+        return False
+
+    count = {}
+
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1
+        else:
+             count[letter] = 1
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+
+    return True 
+    
+
+
+print(inter_anagram('god', 'dog'))
+
+
+
+
+
 
         
