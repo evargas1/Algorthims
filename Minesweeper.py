b4996c15ebc8d83322938aa79532982d072f21a5
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-04-01 12:06:16 -0500

    added the changes from the newly added algo

---
+++ Minesweeper.py
@@ -0,0 +1,13 @@
+# write a function that will take 3 arugments 
+# bombs = list of bomb locations
+# can include rows and columns 
+# mine_sweeper ([0,0], [0,1], 3, 4 )
+# bomb at row index 0 column index 0
+# bomb at row index 0 column index 1
+
+# 3 rows
+# 3 columns 
+
+#  we shoould return an 3 * 4 array (-1) = bomb
+# [[-1, -1, 1, 0]]
+# [2,2,1,0] 2 bombs in surronding areas have also the [0,0,0,0]
